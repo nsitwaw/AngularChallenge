de6dadf355b864e79c145494e950aaab3c7508f3
Łukasz Żurowski <lukasz.zurowski@dmns.pl>
2015-12-11 11:23:07 +0100

    START

---
+++ client/app.js
@@ -0,0 +1,22 @@
+var app = angular.module('app', ['ngRoute', 'ngAnimate', 'ngSanitize', 'mgcrea.ngStrap']);
+
+app.value('app-version', '0.0.1');
+
+app.config(['$locationProvider', '$routeProvider', '$httpProvider', function ($locationProvider, $routeProvider, $httpProvider) {
+
+    $routeProvider
+        .when('/',{
+            templateUrl: 'views/products.html'
+        })
+        .when('/chellenge',{
+            templateUrl: 'views/chellenge.html'
+        })
+        .when('/404', {
+            templateUrl: '404.html'
+        })
+        
+        .otherwise({redirectTo: '/404'})
+    ;
+
+    $locationProvider.html5Mode(true).hashPrefix('!');
+}]);
